a5f6af4c1dbf6aff5a794756b2c1e08002bdf55f
Sven Nilsen <bvssvni@gmail.com>
2015-04-04 21:27:27 +0200

    Deleted some old code

--- src/shape.rs
+++ src/shape.rs
@@ -21,29 +21,3 @@ pub trait Shapeable: Sized {
 /// Size property.
 #[derive(Copy, Clone)]
 pub struct Size(pub Dimensions);
-
-/*
-impl<T> Shapeable for T
-    where
-        (Size, T): Pair<Data = Size, Object = T> + SetAt + GetFrom
-{
-    #[inline]
-    fn dimensions(self, width: f64, height: f64) -> Self {
-        self.set(Size([width, height]))
-    }
-    #[inline]
-    fn dim(self, dim: ::dimensions::Dimensions) -> Self {
-        self.set(Size(dim))
-    }
-    #[inline]
-    fn width(self, width: f64) -> Self {
-        let Size(size) = self.get();
-        self.set(Size([width, size[1]]))
-    }
-    #[inline]
-    fn height(self, height: f64) -> Self {
-        let Size(size) = self.get();
-        self.set(Size([size[0], height]))
-    }
-}
-*/
