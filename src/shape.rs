b6cd2b566f626141c744a604a0eb430a0f0e1842
Sven Nilsen <bvssvni@gmail.com>
2015-04-04 21:24:51 +0200

    Made `Shapeable::dimensions` a provided trait method

--- src/shape.rs
+++ src/shape.rs
@@ -2,10 +2,12 @@ use dimensions::Dimensions;
 
 /// A trait that indicates whether or not a widget
 /// builder is positionable.
-pub trait Shapeable {
+pub trait Shapeable: Sized {
     fn get_dim(&self) -> Dimensions;
     fn dim(self, dim: Dimensions) -> Self;
-    fn dimensions(self, width: f64, height: f64) -> Self;
+    fn dimensions(self, width: f64, height: f64) -> Self {
+        self.dim([width, height])
+    }
     fn width(self, width: f64) -> Self;
     fn height(self, height: f64) -> Self;
 }
