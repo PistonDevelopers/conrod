8f33b2149ce895686633b3fed2db202e36f0e91f
mitchmindtree <mitchell.nordine@gmail.com>
2015-04-22 04:02:50 +1000

    Exposed mouse.rs module, fixed handling of right and middle clicks

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,20 +1,33 @@
+//! 
+//! A module for describing Mouse state.
+//!
+//! The `Ui` will continuously maintain the latest Mouse state, necessary for widget logic.
+//!
 
 use position::Point;
 
 /// Represents the current state of a mouse button.
 #[derive(Clone, Copy, Debug)]
 pub enum ButtonState {
+    /// The mouse is currently up.
     Up,
+    /// The mouse is currently down (pressed).
     Down,
 }
 
 /// Represents the current state of the Mouse.
 #[derive(Copy, Clone, Debug)]
 pub struct Mouse {
+    /// Position of the mouse cursor.
     pub xy: Point,
+    /// Left mouse button state.
     pub left: ButtonState,
+    /// Middle mouse button state.
     pub middle: ButtonState,
+    /// Right mouse button state.
     pub right: ButtonState,
+    /// Unknown button state.
+    pub unknown: ButtonState,
 }
 
 impl Mouse {
@@ -24,7 +37,7 @@ impl Mouse {
                left: ButtonState,
                middle: ButtonState,
                right: ButtonState) -> Mouse {
-        Mouse { xy: xy, left: left, middle: middle, right: right }
+        Mouse { xy: xy, left: left, middle: middle, right: right, unknown: ButtonState::Up }
     }
 
     /// Return the mouse state with its position relative to the given position.
