efc0c5a3305f4f3e56e4299862e8950b62fc2972
Sven Nilsen <bvssvni@gmail.com>
2015-05-02 02:05:03 +0200

    Made custom widgets use `Box<Any>`
    Closes https://github.com/PistonDevelopers/conrod/issues/397
    
    * Removed `State` trait
    * Removed `Custom::State`
    * Removed `Copy/Clone` on `Kind/Widget`
    * Added `#![feature(core)]` to check type id, will be removed when
    `Any` gets associated static

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-//! 
+//!
 //! # Conrod
 //!
 //! An easy-to-use, immediate-mode, 2D GUI library featuring a range of useful widgets.
 //!
 
 #![deny(missing_copy_implementations)]
+#![feature(core)]
 #![warn(missing_docs)]
 
 #[macro_use] extern crate bitflags;
@@ -20,7 +21,6 @@ extern crate vecmath;
 
 pub use widget::button::Button;
 pub use widget::custom::Custom as CustomWidget;
-pub use widget::custom::State as CustomWidgetState;
 pub use widget::drop_down_list::DropDownList;
 pub use widget::envelope_editor::EnvelopeEditor;
 pub use widget::envelope_editor::EnvelopePoint;
@@ -31,6 +31,7 @@ pub use widget::text_box::TextBox;
 pub use widget::toggle::Toggle;
 pub use widget::matrix::Matrix as WidgetMatrix;
 pub use widget::xy_pad::XYPad;
+pub use widget::Kind as WidgetKind;
 
 pub use background::Background;
 pub use elmesque::color;
@@ -57,4 +58,3 @@ mod theme;
 mod ui;
 mod utils;
 mod widget;
-
