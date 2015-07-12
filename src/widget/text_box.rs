105942e8b35431defd40b7982af0d24681fcae38
mitchmindtree <mitchell.nordine@gmail.com>
2015-07-12 18:50:24 +1000

    Updated TextBox to new CommonBuilder changes

--- src/widget/text_box.rs
+++ src/widget/text_box.rs
@@ -23,23 +23,17 @@ const TEXT_PADDING: Scalar = 5.0;
 /// A widget for displaying and mutating a given one-line text `String`. It's reaction is
 /// triggered upon pressing of the `Enter`/`Return` key.
 pub struct TextBox<'a, F> {
+    common: widget::CommonBuilder,
     text: &'a mut String,
-    pos: Position,
-    maybe_h_align: Option<HorizontalAlign>,
-    maybe_v_align: Option<VerticalAlign>,
-    depth: Depth,
     maybe_react: Option<F>,
     style: Style,
     enabled: bool,
-    maybe_parent_id: Option<WidgetId>,
 }
 
 /// Styling for the TextBox, necessary for constructing its renderable Element.
 #[allow(missing_docs, missing_copy_implementations)]
 #[derive(Clone, Debug, PartialEq, RustcEncodable, RustcDecodable)]
 pub struct Style {
-    pub maybe_width: Option<Scalar>,
-    pub maybe_height: Option<Scalar>,
     pub maybe_color: Option<Color>,
     pub maybe_frame: Option<Scalar>,
     pub maybe_frame_color: Option<Color>,
@@ -293,15 +287,11 @@ impl<'a, F> TextBox<'a, F> {
     /// Construct a TextBox widget.
     pub fn new(text: &'a mut String) -> TextBox<'a, F> {
         TextBox {
+            common: widget::CommonBuilder::new(),
             text: text,
-            pos: Position::default(),
-            maybe_h_align: None,
-            maybe_v_align: None,
-            depth: 0.0,
             maybe_react: None,
             style: Style::new(),
             enabled: true,
-            maybe_parent_id: None,
         }
     }
 
@@ -324,13 +314,6 @@ impl<'a, F> TextBox<'a, F> {
         self
     }
 
-    /// Set which parent to attach the Widget to. Note that you can also attach a widget to a
-    /// parent by using the placement `Positionable` methods.
-    pub fn parent(mut self, id: WidgetId) -> Self {
-        self.maybe_parent_id = Some(id);
-        self
-    }
-
 }
 
 impl<'a, F> Widget for TextBox<'a, F>
@@ -339,6 +322,8 @@ impl<'a, F> Widget for TextBox<'a, F>
 {
     type State = State;
     type Style = Style;
+    fn common(&self) -> &CommonBuilder { &self.common }
+    fn common_mut(&mut self) -> &mut CommonBuilder { &mut self.common }
     fn unique_kind(&self) -> &'static str { "TextBox" }
     fn init_state(&self) -> State {
         State {
@@ -347,7 +332,20 @@ impl<'a, F> Widget for TextBox<'a, F>
         }
     }
     fn style(&self) -> Style { self.style.clone() }
-    fn parent_id(&self) -> Option<WidgetId> { self.maybe_parent_id }
+
+    fn default_width<C: CharacterCache>(&self, theme: &Theme, _: &GlyphCache<C>) -> Scalar {
+        const DEFAULT_WIDTH: Scalar = 192.0;
+        self.maybe_width.or(theme.maybe_text_box.as_ref().map(|default| {
+            default.common.maybe_width.unwrap_or(DEFAULT_WIDTH)
+        })).unwrap_or(DEFAULT_WIDTH)
+    }
+
+    fn default_height(&self, theme: &Theme) -> Scalar {
+        const DEFAULT_HEIGHT: Scalar = 48.0;
+        self.maybe_height.or(theme.maybe_text_box.as_ref().map(|default| {
+            default.common.maybe_height.unwrap_or(DEFAULT_HEIGHT)
+        })).unwrap_or(DEFAULT_HEIGHT)
+    }
 
     /// Capture the keyboard if the Interaction has become `Captured`.
     fn capture_keyboard(prev: &State, new: &State) -> bool {
@@ -586,29 +584,11 @@ impl Style {
     /// Construct the default Style.
     pub fn new() -> Style {
         Style {
-            maybe_width: None,
-            maybe_height: None,
             maybe_color: None,
             maybe_frame: None,
             maybe_frame_color: None,
             maybe_font_size: None,
         }
-    }
-
-    /// Get the width of the Widget.
-    pub fn width(&self, theme: &Theme) -> Scalar {
-        const DEFAULT_WIDTH: Scalar = 192.0;
-        self.maybe_width.or(theme.maybe_text_box.as_ref().map(|style| {
-            style.maybe_width.unwrap_or(DEFAULT_WIDTH)
-        })).unwrap_or(DEFAULT_WIDTH)
-    }
-
-    /// Get the height of the Widget.
-    pub fn height(&self, theme: &Theme) -> Scalar {
-        const DEFAULT_HEIGHT: Scalar = 48.0;
-        self.maybe_height.or(theme.maybe_text_box.as_ref().map(|style| {
-            style.maybe_height.unwrap_or(DEFAULT_HEIGHT)
-        })).unwrap_or(DEFAULT_HEIGHT)
     }
 
     /// Get the Color for an Element.
@@ -660,48 +640,3 @@ impl<'a, F> Frameable for TextBox<'a, F> {
     }
 }
 
-impl<'a, F> position::Positionable for TextBox<'a, F> {
-    fn position(mut self, pos: Position) -> Self {
-        self.pos = pos;
-        self
-    }
-    fn get_position(&self) -> Position { self.pos }
-    #[inline]
-    fn horizontal_align(self, h_align: HorizontalAlign) -> Self {
-        TextBox { maybe_h_align: Some(h_align), ..self }
-    }
-    #[inline]
-    fn vertical_align(self, v_align: VerticalAlign) -> Self {
-        TextBox { maybe_v_align: Some(v_align), ..self }
-    }
-    fn get_horizontal_align(&self, theme: &Theme) -> HorizontalAlign {
-        self.maybe_h_align.unwrap_or(theme.align.horizontal)
-    }
-    fn get_vertical_align(&self, theme: &Theme) -> VerticalAlign {
-        self.maybe_v_align.unwrap_or(theme.align.vertical)
-    }
-    fn depth(mut self, depth: Depth) -> Self {
-        self.depth = depth;
-        self
-    }
-    fn get_depth(&self) -> Depth { self.depth }
-}
-
-impl<'a, F> position::Sizeable for TextBox<'a, F> {
-    #[inline]
-    fn width(mut self, w: Scalar) -> Self {
-        self.style.maybe_width = Some(w);
-        self
-    }
-    #[inline]
-    fn height(mut self, h: Scalar) -> Self {
-        self.style.maybe_height = Some(h);
-        self
-    }
-    fn get_width<C: CharacterCache>(&self, theme: &Theme, _: &GlyphCache<C>) -> Scalar {
-        self.style.width(theme)
-    }
-    fn get_height(&self, theme: &Theme) -> Scalar {
-        self.style.height(theme)
-    }
-}
