d37a543bfe07f8cdf8af8a4ba0113574a044a115
mitchmindtree <mitchell.nordine@gmail.com>
2015-10-19 22:29:54 +1100

    Update example to latest Widget::update and input capturing changes.

--- examples/custom_widget.rs
+++ examples/custom_widget.rs
@@ -42,7 +42,6 @@ mod circular_button {
         Theme,
         UpdateArgs,
         Widget,
-        WidgetState,
     };
 
 
@@ -204,30 +203,6 @@ mod circular_button {
         }
         fn style(&self) -> Style { self.style.clone() }
 
-        /// Whether the button should capture the mouse. Widgets can "capture" user input.
-        /// If the button captures the mouse, then mouse events will only be seen by the
-        /// button. Other widgets will not see mouse events until the button uncaptures
-        /// the mouse.
-        fn capture_mouse(prev: &State, new: &State) -> bool {
-            match (prev.interaction, new.interaction) {
-                // If the user has pressed the button since the last update, then the
-                // button captures the mouse.
-                (Interaction::Highlighted, Interaction::Clicked) => true,
-                _ => false,
-            }
-        }
-
-        /// Whether the button should uncapture the mouse.
-        fn uncapture_mouse(prev: &State, new: &State) -> bool {
-            match (prev.interaction, new.interaction) {
-                // If the user has released the button since the last update, then the
-                // button uncaptures the mouse.
-                (Interaction::Clicked, Interaction::Highlighted) => true,
-                (Interaction::Clicked, Interaction::Normal) => true,
-                _ => false,
-            }
-        }
-
         /// Default width of the widget. This method is optional. The Widget trait
         /// provides a default implementation that always returns zero.
         fn default_width<C: CharacterCache>(&self, theme: &Theme, _: &GlyphCache<C>) -> Scalar {
@@ -254,11 +229,8 @@ mod circular_button {
         /// Update the state of the button. The state may or may not have changed since
         /// the last update. (E.g. it may have changed because the user moused over the
         /// button.) If the state has changed, return the new state. Else, return None.
-        fn update<'b, C>(mut self, args: UpdateArgs<'b, Self, C>) -> Option<State>
-            where C: CharacterCache,
-        {
-            let UpdateArgs { prev_state, rect, ui, .. } = args;
-            let WidgetState { ref state, .. } = *prev_state;
+        fn update<C: CharacterCache>(mut self, args: UpdateArgs<Self, C>) {
+            let UpdateArgs { state, rect, mut ui, .. } = args;
             let (xy, dim) = rect.xy_dim();
             let maybe_mouse = ui.input().maybe_mouse.map(|mouse| mouse.relative_to(xy));
 
@@ -275,7 +247,7 @@ mod circular_button {
                     let is_over = is_over_circ([0.0, 0.0], mouse.xy, dim);
 
                     // See above where we define get_new_interaction.
-                    get_new_interaction(is_over, state.interaction, mouse)
+                    get_new_interaction(is_over, state.view().interaction, mouse)
                 },
             };
 
@@ -284,7 +256,7 @@ mod circular_button {
             // of right now. So this if statement is saying: If the button was clicked a
             // moment ago, and it's now highlighted, then the button has been activated.
             if let (Interaction::Clicked, Interaction::Highlighted) =
-                (state.interaction, new_interaction)
+                (state.view().interaction, new_interaction)
             {
                 // Recall that our CircularButton struct includes maybe_react, which
                 // stores either a reaction function or None. If maybe_react is Some, call
@@ -292,41 +264,48 @@ mod circular_button {
                 if let Some(ref mut react) = self.maybe_react { react() }
             }
 
-            // A function for constructing a new state. We'll call this function below
-            // if we determine that the state has changed.
-            let new_state = || State {
-                // Recall that our CircularButton struct includes maybe_label, which
-                // stores either an &str or None. If there's something there, we
-                // copy the &str into an owned String for the State object.
-                maybe_label: self.maybe_label.as_ref().map(|label| label.to_string()),
+            // Here we check to see whether or not our button should capture the mouse.
+            //
+            // Widgets can "capture" user input. If the button captures the mouse, then mouse
+            // events will only be seen by the button. Other widgets will not see mouse events
+            // until the button uncaptures the mouse.
+            match (state.view().interaction, new_interaction) {
+                // If the user has pressed the button we capture the mouse.
+                (Interaction::Highlighted, Interaction::Clicked) => {
+                    ui.capture_mouse();
+                },
+                // If the user releases the button, we uncapture the mouse.
+                (Interaction::Clicked, Interaction::Highlighted) |
+                (Interaction::Clicked, Interaction::Normal)      => {
+                    ui.uncapture_mouse();
+                },
+                _ => (),
+            }
 
-                // We constructed new_interaction above. This is the interaction state
-                // as of this update.
-                interaction: new_interaction,
-            };
+            // Whenever we call `state.update` (as below), a flag is set within our `State`
+            // indicating that there has been some mutation and that our widget requires a
+            // new `Element` (meaning that `Widget::draw` will be called again). Thus, we only want
+            // to call `state.update` if there has been some change in order to only redraw our
+            // `Element` when absolutely required.
+            //
+            // You can see how we do this below - we check if the state has changed before calling
+            // `state.update`.
 
-            // Check whether or not the state has changed since the previous update. The
-            // state has changed if (1) the interaction state has changed, or (2) the
-            // label string has changed.
-            let state_has_changed =
-                state.interaction != new_interaction
-                || state.maybe_label
-                    // Convert the Option<String> to an Option<&String>.
-                    .as_ref()
-                    // Convert the Option<&String> to an Option<&str>.
-                    .map(|string| &string[..])
-                    // Compare the current Option<&str> to the previous one.
-                    != self.maybe_label;
+            // If the interaction has changed, set the new interaction.
+            if state.view().interaction != new_interaction {
+                state.update(|state| state.interaction = new_interaction);
+            }
 
-            // Construct and return the new state if there was a change. Recall that we
-            // defined new_state above.
-            if state_has_changed { Some(new_state()) } else { None }
+            // If the label has changed, set the new label.
+            if state.view().maybe_label.as_ref().map(|label| &label[..]) != self.maybe_label {
+                state.update(|state| {
+                    state.maybe_label = self.maybe_label.as_ref().map(|label| label.to_string());
+                })
+            }
         }
 
         /// Construct and return a renderable `Element` for the given button state.
-        fn draw<'b, C>(args: DrawArgs<'b, Self, C>) -> Element
-            where C: CharacterCache,
-        {
+        fn draw<C: CharacterCache>(args: DrawArgs<Self, C>) -> Element {
             use elmesque;
             use elmesque::form::{collage, circle, text};
 
