d245910b7e4bc74fe27999ee42a1cbc3264bf80f
pfried <philipsfried@gmail.com>
2016-01-21 12:53:57 -0500

    extracted event traits in preparation for filtered and relative events

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -34,6 +34,115 @@ pub struct MouseClick {
     modifier: ModifierKey,
 }
 
+#[allow(missing_docs)]
+pub trait EventProvider {
+    fn all_events(&self) -> &Vec<ConrodEvent>;
+    fn modifiers(&self) -> ModifierKey;
+
+    fn text_just_entered(&self) -> Option<String> {
+        let all_text: String = self.all_events().iter().filter_map(|evt| {
+            match *evt {
+                ConrodEvent::Raw(Input::Text(ref text)) => Some(text),
+                _ => None
+            }
+        }).fold(String::new(), |acc, item| {
+            acc + item
+        });
+
+        if all_text.is_empty() {
+            None
+        } else {
+            Some(all_text)
+        }
+    }
+
+    fn keys_just_released(&self) -> Vec<Key> {
+        use input::Button::Keyboard;
+
+        self.all_events().iter().filter_map(|evt| {
+            match *evt {
+                ConrodEvent::Raw(Input::Release(Keyboard(key))) => Some(key),
+                _ => None
+            }
+        }).collect::<Vec<Key>>()
+    }
+
+    fn keys_just_pressed(&self) -> Vec<Key> {
+        use input::Button::Keyboard;
+
+        self.all_events().iter().filter_map(|evt| {
+            match *evt {
+                ConrodEvent::Raw(Input::Press(Keyboard(key))) => Some(key),
+                _ => None
+            }
+        }).collect::<Vec<Key>>()
+    }
+
+    fn scroll(&self) -> Option<Scroll> {
+        self.all_events().iter().filter_map(|evt| {
+            match *evt {
+                ConrodEvent::Raw(Input::Move(Motion::MouseScroll(x, y))) => {
+                    Some(Scroll{
+                        x: x,
+                        y: y,
+                        modifiers: self.modifiers()
+                    })
+                },
+                _ => None
+            }
+        }).fold(None, |maybe_scroll, scroll| {
+            if maybe_scroll.is_some() {
+                maybe_scroll.map(|acc| {
+                    Scroll{
+                        x: acc.x + scroll.x,
+                        y: acc.y + scroll.y,
+                        modifiers: self.modifiers()
+                    }
+                })
+            } else {
+                Some(scroll)
+            }
+        })
+    }
+
+    fn mouse_left_drag(&self) -> Option<MouseDrag> {
+        self.mouse_drag(MouseButton::Left)
+    }
+
+    fn mouse_drag(&self, button: MouseButton) -> Option<MouseDrag> {
+        self.all_events().iter().filter_map(|evt| {
+            match *evt {
+                ConrodEvent::MouseDrag(drag_evt) if drag_evt.button == button => Some(drag_evt),
+                _ => None
+            }
+        }).last()
+    }
+
+    fn mouse_left_click(&self) -> Option<MouseClick> {
+        self.mouse_click(MouseButton::Left)
+    }
+
+    fn mouse_right_click(&self) -> Option<MouseClick> {
+        self.mouse_click(MouseButton::Right)
+    }
+
+    fn mouse_click(&self, button: MouseButton) -> Option<MouseClick> {
+        self.all_events().iter().filter_map(|evt| {
+            match *evt {
+                ConrodEvent::MouseClick(click) if click.button == button => Some(click),
+                _ => None
+            }
+        }).next()
+    }
+
+}
+
+#[allow(missing_docs)]
+pub trait EventAggregator: EventProvider {
+    fn push_event(&mut self, event: ConrodEvent);
+    fn reset(&mut self);
+}
+
 #[derive(Copy, Clone, PartialEq, Debug)]
 #[allow(missing_docs)]
 pub struct Scroll {
@@ -43,7 +152,7 @@ pub struct Scroll {
 }
 
 #[allow(missing_docs)]
-pub struct WidgetEvents {
+pub struct ConrodEventAggregator {
     events: Vec<ConrodEvent>,
     mouse_buttons: ButtonMap,
     mouse_position: Point,
@@ -51,20 +160,20 @@ pub struct WidgetEvents {
     modifiers: ModifierKey,
 }
 
-#[allow(missing_docs)]
-impl WidgetEvents {
+impl EventProvider for ConrodEventAggregator {
 
-    pub fn new() -> WidgetEvents {
-        WidgetEvents{
-            events: Vec::new(),
-            mouse_buttons: ButtonMap::new(),
-            mouse_position: [0.0, 0.0],
-            drag_threshold: 4.0,
-            modifiers: ModifierKey::default(),
-        }
+    fn modifiers(&self) -> ModifierKey {
+        self.modifiers
     }
 
-    pub fn push_event(&mut self, event: ConrodEvent) {
+    fn all_events(&self) -> &Vec<ConrodEvent> {
+        &self.events
+    }
+}
+
+impl EventAggregator for ConrodEventAggregator {
+
+    fn push_event(&mut self, event: ConrodEvent) {
         use input::Input::{Press, Release, Move};
         use input::Motion::MouseCursor;
         use input::Button::Mouse;
@@ -84,111 +193,24 @@ impl WidgetEvents {
         }
     }
 
-    pub fn reset(&mut self) {
+    fn reset(&mut self) {
         self.events.clear();
     }
 
-    pub fn all_events(&self) -> &Vec<ConrodEvent> {
-        &self.events
-    }
+}
 
-    pub fn text_just_entered(&self) -> Option<String> {
-        let all_text: String = self.all_events().iter().filter_map(|evt| {
-            match *evt {
-                ConrodEvent::Raw(Input::Text(ref text)) => Some(text),
-                _ => None
-            }
-        }).fold(String::new(), |acc, item| {
-            acc + item
-        });
+#[allow(missing_docs)]
+impl ConrodEventAggregator {
 
-        if all_text.is_empty() {
-            None
-        } else {
-            Some(all_text)
+    pub fn new() -> ConrodEventAggregator {
+        ConrodEventAggregator{
+            events: Vec::new(),
+            mouse_buttons: ButtonMap::new(),
+            mouse_position: [0.0, 0.0],
+            drag_threshold: 4.0,
+            modifiers: ModifierKey::default(),
         }
     }
-
-    pub fn keys_just_released(&self) -> Vec<Key> {
-        use input::Button::Keyboard;
-
-        self.all_events().iter().filter_map(|evt| {
-            match *evt {
-                ConrodEvent::Raw(Input::Release(Keyboard(key))) => Some(key),
-                _ => None
-            }
-        }).collect::<Vec<Key>>()
-    }
-
-    pub fn keys_just_pressed(&self) -> Vec<Key> {
-        use input::Button::Keyboard;
-
-        self.all_events().iter().filter_map(|evt| {
-            match *evt {
-                ConrodEvent::Raw(Input::Press(Keyboard(key))) => Some(key),
-                _ => None
-            }
-        }).collect::<Vec<Key>>()
-    }
-
-    pub fn scroll(&self) -> Option<Scroll> {
-        self.all_events().iter().filter_map(|evt| {
-            match *evt {
-                ConrodEvent::Raw(Input::Move(Motion::MouseScroll(x, y))) => {
-                    Some(Scroll{
-                        x: x,
-                        y: y,
-                        modifiers: self.modifiers
-                    })
-                },
-                _ => None
-            }
-        }).fold(None, |maybe_scroll, scroll| {
-            if maybe_scroll.is_some() {
-                maybe_scroll.map(|acc| {
-                    Scroll{
-                        x: acc.x + scroll.x,
-                        y: acc.y + scroll.y,
-                        modifiers: self.modifiers
-                    }
-                })
-            } else {
-                Some(scroll)
-            }
-        })
-    }
-
-    pub fn mouse_left_drag(&self) -> Option<MouseDrag> {
-        self.mouse_drag(MouseButton::Left)
-    }
-
-    pub fn mouse_drag(&self, button: MouseButton) -> Option<MouseDrag> {
-        self.all_events().iter().filter_map(|evt| {
-            match *evt {
-                ConrodEvent::MouseDrag(drag_evt) if drag_evt.button == button => Some(drag_evt),
-                _ => None
-            }
-        }).last()
-    }
-
-    pub fn mouse_left_click(&self) -> Option<MouseClick> {
-        self.mouse_click(MouseButton::Left)
-    }
-
-    pub fn mouse_right_click(&self) -> Option<MouseClick> {
-        self.mouse_click(MouseButton::Right)
-    }
-
-    pub fn mouse_click(&self, button: MouseButton) -> Option<MouseClick> {
-        self.all_events().iter().filter_map(|evt| {
-            match *evt {
-                ConrodEvent::MouseClick(click) if click.button == button => Some(click),
-                _ => None
-            }
-        }).next()
-    }
-
-
 
     fn handle_mouse_move(&mut self, move_to: Point) -> Option<ConrodEvent> {
         self.mouse_position = move_to;
@@ -262,7 +284,7 @@ impl WidgetEvents {
     }
 }
 
-impl IntoIterator for WidgetEvents {
+impl IntoIterator for ConrodEventAggregator {
     type Item = ConrodEvent;
     type IntoIter = ::std::vec::IntoIter<ConrodEvent>;
 
--- src/events/tests.rs
+++ src/events/tests.rs
@@ -8,50 +8,50 @@ use super::*;
 
 #[test]
 fn entered_text_should_be_aggregated_from_multiple_events() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Text("Phil ".to_string())));
-    handler.push_event(ConrodEvent::Raw(Input::Text("is a".to_string())));
-    handler.push_event(ConrodEvent::Raw(Input::Text("wesome".to_string())));
+    aggregator.push_event(ConrodEvent::Raw(Input::Text("Phil ".to_string())));
+    aggregator.push_event(ConrodEvent::Raw(Input::Text("is a".to_string())));
+    aggregator.push_event(ConrodEvent::Raw(Input::Text("wesome".to_string())));
 
-    let actual_text = handler.text_just_entered().expect("expected to get a String, got None");
+    let actual_text = aggregator.text_just_entered().expect("expected to get a String, got None");
     assert_eq!("Phil is awesome".to_string(), actual_text);
 }
 
 #[test]
 fn drag_event_should_still_be_created_if_reset_is_called_between_press_and_release() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
-    handler.push_event(mouse_move_event(50.0, 77.7));
-    handler.reset();
-    handler.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
+    aggregator.push_event(mouse_move_event(50.0, 77.7));
+    aggregator.reset();
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
 
-    assert!(handler.mouse_left_drag().is_some());
+    assert!(aggregator.mouse_left_drag().is_some());
 }
 #[test]
 fn click_event_should_still_be_created_if_reset_is_called_between_press_and_release() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
-    handler.reset();
-    handler.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
+    aggregator.reset();
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
 
-    assert!(handler.mouse_left_click().is_some());
+    assert!(aggregator.mouse_left_click().is_some());
 }
 
 #[test]
 fn no_events_should_be_returned_after_reset_is_called() {
-    let mut handler = WidgetEvents::new();
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::RShift))));
-    handler.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(7.0, 88.5))));
-    handler.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
-    handler.push_event(mouse_move_event(60.0, 30.0));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
+    let mut aggregator = ConrodEventAggregator::new();
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::RShift))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(7.0, 88.5))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
+    aggregator.push_event(mouse_move_event(60.0, 30.0));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
 
-    handler.reset();
+    aggregator.reset();
 
-    let events = handler.all_events();
+    let events = aggregator.all_events();
     assert!(events.is_empty());
 }
 
@@ -59,11 +59,11 @@ fn no_events_should_be_returned_after_reset_is_called() {
 fn drag_with_modifer_key_should_include_modifiers_in_drag_event() {
     use input::keyboard::SHIFT;
 
-    let mut handler = WidgetEvents::new();
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::RShift))));
-    handler.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(7.0, 88.5))));
+    let mut aggregator = ConrodEventAggregator::new();
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::RShift))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(7.0, 88.5))));
 
-    let scroll = handler.scroll().expect("expected a scroll event");
+    let scroll = aggregator.scroll().expect("expected a scroll event");
 
     assert_eq!(SHIFT, scroll.modifiers);
 }
@@ -72,12 +72,12 @@ fn drag_with_modifer_key_should_include_modifiers_in_drag_event() {
 fn click_with_modifier_key_should_include_modifiers_in_click_event() {
     use input::keyboard::CTRL;
 
-    let mut handler = WidgetEvents::new();
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::LCtrl))));
-    handler.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
+    let mut aggregator = ConrodEventAggregator::new();
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::LCtrl))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
 
-    let click = handler.mouse_left_click().expect("expected mouse left click event");
+    let click = aggregator.mouse_left_click().expect("expected mouse left click event");
     let expected = MouseClick {
         button: MouseButton::Left,
         location: [0.0, 0.0],
@@ -88,40 +88,40 @@ fn click_with_modifier_key_should_include_modifiers_in_click_event() {
 
 #[test]
 fn keys_just_released_should_return_vec_of_keys_just_released() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::D))));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::O))));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::R))));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::K))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::D))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::O))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::R))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Keyboard(Key::K))));
 
     let expected = vec![Key::D, Key::O, Key::R, Key::K];
-    let actual = handler.keys_just_released();
+    let actual = aggregator.keys_just_released();
     assert_eq!(expected, actual);
 
 }
 
 #[test]
 fn keys_just_pressed_should_return_vec_of_keys_just_pressed() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::N))));
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::E))));
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::R))));
-    handler.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::D))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::N))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::E))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::R))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Keyboard(Key::D))));
 
     let expected = vec![Key::N, Key::E, Key::R, Key::D];
-    let actual = handler.keys_just_pressed();
+    let actual = aggregator.keys_just_pressed();
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn scroll_events_should_be_aggregated_into_one_when_scroll_is_called() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
-    handler.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
-    handler.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
 
     let expected_scroll = Scroll {
         x: 30.0,
@@ -129,32 +129,32 @@ fn scroll_events_should_be_aggregated_into_one_when_scroll_is_called() {
         modifiers: ModifierKey::default()
     };
 
-    let actual = handler.scroll().expect("expected a scroll event");
+    let actual = aggregator.scroll().expect("expected a scroll event");
     assert_eq!(expected_scroll, actual);
 
 }
 
 #[test]
 fn handler_should_return_scroll_event_if_one_exists() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Move(Motion::MouseScroll(10.0, 33.0))));
 
     let expected_scroll = Scroll{
         x: 10.0,
         y: 33.0,
         modifiers: ModifierKey::default()
     };
-    let actual_scroll = handler.scroll().expect("expected a scroll event");
+    let actual_scroll = aggregator.scroll().expect("expected a scroll event");
     assert_eq!(expected_scroll, actual_scroll);
 }
 #[test]
 fn mouse_button_pressed_moved_released_creates_final_drag_event() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
-    handler.push_event(mouse_move_event(20.0, 10.0));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
+    aggregator.push_event(mouse_move_event(20.0, 10.0));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
 
     let expected_drag = MouseDrag{
         button: MouseButton::Left,
@@ -163,18 +163,18 @@ fn mouse_button_pressed_moved_released_creates_final_drag_event() {
         modifier: ModifierKey::default(),
         in_progress: false
     };
-    let mouse_drag = handler.mouse_drag(MouseButton::Left).expect("Expected to get a mouse drag event");
+    let mouse_drag = aggregator.mouse_drag(MouseButton::Left).expect("Expected to get a mouse drag event");
     assert_eq!(expected_drag, mouse_drag);
 }
 
 #[test]
 fn mouse_button_pressed_then_moved_creates_drag_event() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
     let press = ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left)));
     let mouse_move = mouse_move_event(20.0, 10.0);
-    handler.push_event(press.clone());
-    handler.push_event(mouse_move.clone());
+    aggregator.push_event(press.clone());
+    aggregator.push_event(mouse_move.clone());
 
     let expected_drag = MouseDrag{
         button: MouseButton::Left,
@@ -183,25 +183,25 @@ fn mouse_button_pressed_then_moved_creates_drag_event() {
         modifier: ModifierKey::default(),
         in_progress: true
     };
-    let mouse_drag = handler.mouse_drag(MouseButton::Left).expect("Expected to get a mouse drag event");
+    let mouse_drag = aggregator.mouse_drag(MouseButton::Left).expect("Expected to get a mouse drag event");
     assert_eq!(expected_drag, mouse_drag);
 }
 
 #[test]
 fn mouse_click_position_should_be_mouse_position_when_pressed() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
-    handler.push_event(mouse_move_event(4.0, 5.0));
-    handler.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
-    handler.push_event(mouse_move_event(5.0, 5.0));
-    handler.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
+    aggregator.push_event(mouse_move_event(4.0, 5.0));
+    aggregator.push_event(ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left))));
+    aggregator.push_event(mouse_move_event(5.0, 5.0));
+    aggregator.push_event(ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left))));
 
     let expected_click = MouseClick {
         button: MouseButton::Left,
         location: [4.0, 5.0],
         modifier: ModifierKey::default()
     };
-    let actual_click = handler.mouse_click(MouseButton::Left).expect("expected a mouse click event");
+    let actual_click = aggregator.mouse_click(MouseButton::Left).expect("expected a mouse click event");
 
     assert_eq!(expected_click, actual_click);
 
@@ -209,33 +209,33 @@ fn mouse_click_position_should_be_mouse_position_when_pressed() {
 
 #[test]
 fn mouse_button_pressed_then_released_should_create_mouse_click_event() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
     let press = ConrodEvent::Raw(Input::Press(Mouse(MouseButton::Left)));
     let release = ConrodEvent::Raw(Input::Release(Mouse(MouseButton::Left)));
-    handler.push_event(press.clone());
-    handler.push_event(release.clone());
+    aggregator.push_event(press.clone());
+    aggregator.push_event(release.clone());
 
     let expected_click = MouseClick {
         button: MouseButton::Left,
         location: [0.0, 0.0],
         modifier: ModifierKey::default()
     };
-    let actual_click = handler.mouse_click(MouseButton::Left).expect("expected a mouse click event");
+    let actual_click = aggregator.mouse_click(MouseButton::Left).expect("expected a mouse click event");
 
     assert_eq!(expected_click, actual_click);
 }
 
 #[test]
 fn all_events_should_return_all_inputs_in_order() {
-    let mut handler = WidgetEvents::new();
+    let mut aggregator = ConrodEventAggregator::new();
 
     let evt1 = ConrodEvent::Raw(Input::Press(Keyboard(Key::Z)));
-    handler.push_event(evt1.clone());
+    aggregator.push_event(evt1.clone());
     let evt2 = ConrodEvent::Raw(Input::Press(Keyboard(Key::A)));
-    handler.push_event(evt2.clone());
+    aggregator.push_event(evt2.clone());
 
-    let results = handler.all_events();
+    let results = aggregator.all_events();
     assert_eq!(2, results.len());
     assert_eq!(evt1, results[0]);
     assert_eq!(evt2, results[1]);
