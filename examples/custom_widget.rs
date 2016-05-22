357d3b02e5cc1dab1aefe0ae6ec2bfbdb63ac948
mitchmindtree <mitchell.nordine@gmail.com>
2016-05-22 20:03:27 +1000

    Update the custom_widget example to both the widget_style macro as well as the new event system.

--- examples/custom_widget.rs
+++ examples/custom_widget.rs
@@ -29,12 +29,9 @@ mod circular_button {
         FontSize,
         IndexSlot,
         Labelable,
-        Mouse,
         Point,
         Positionable,
-        Scalar,
         Text,
-        Theme,
         UpdateArgs,
         Widget,
         WidgetKind,
@@ -57,91 +54,34 @@ mod circular_button {
         enabled: bool
     }
 
-    /// Represents the unique styling for our CircularButton widget.
-    #[derive(Clone, Debug, PartialEq)]
-    pub struct Style {
-        /// Color of the button.
-        pub maybe_color: Option<Color>,
-        /// Radius of the button.
-        pub maybe_radius: Option<Scalar>,
-        /// Color of the button's label.
-        pub maybe_label_color: Option<Color>,
-        /// Font size of the button's label.
-        pub maybe_label_font_size: Option<u32>,
+    /// A `&'static str` that can be used to uniquely identify our widget type.
+    pub const KIND: WidgetKind = "CircularButton";
+
+    // We use the `widget_style!` macro to vastly simplify the definition and implementation of the
+    // widget's associated `Style` type. This generates both a `Style` struct, as well as an
+    // implementation that automatically retrieves defaults from the provided theme.
+    //
+    // See the documenation of the macro for a more details.
+    widget_style!{
+        KIND;
+        /// Represents the unique styling for our CircularButton widget.
+        style Style {
+            /// Color of the button.
+            - color: Color { theme.shape_color }
+            /// Color of the button's label.
+            - label_color: Color { theme.label_color }
+            /// Font size of the button's label.
+            - label_font_size: FontSize { theme.font_size_medium }
+        }
     }
 
     /// Represents the unique, cached state for our CircularButton widget.
     #[derive(Clone, Debug, PartialEq)]
     pub struct State {
-        /// The current interaction state. See the Interaction enum below. See also
-        /// get_new_interaction below, where we define all the logic for transitioning between
-        /// interaction states.
-        interaction: Interaction,
         /// An index to use for our **Circle** primitive graphics widget.
         circle_idx: IndexSlot,
         /// An index to use for our **Text** primitive graphics widget (for the label).
         text_idx: IndexSlot,
-    }
-
-    /// A `&'static str` that can be used to uniquely identify our widget type.
-    pub const KIND: WidgetKind = "CircularButton";
-
-    /// A type to keep track of interaction between updates.
-    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-    pub enum Interaction {
-        Normal,
-        Highlighted,
-        Clicked,
-    }
-
-    impl Interaction {
-        /// Alter the widget color depending on the current interaction.
-        fn color(&self, color: Color) -> Color {
-            match *self {
-                /// The base color as defined in the Style struct, or a default provided
-                /// by the current Theme if the Style has no color.
-                Interaction::Normal => color,
-                /// The Color object (from Elmesque) can calculate a highlighted version
-                /// of itself. We don't have to use it, though. We could specify any color
-                /// we want.
-                Interaction::Highlighted => color.highlighted(),
-                /// Ditto for clicked.
-                Interaction::Clicked => color.clicked(),
-            }
-        }
-    }
-
-    /// Check the current interaction with the button. Takes into account whether the mouse is
-    /// over the button and the previous interaction state.
-    fn get_new_interaction(is_over: bool, prev: Interaction, mouse: Mouse) -> Interaction {
-        use conrod::MouseButtonPosition::{Down, Up};
-        use self::Interaction::{Normal, Highlighted, Clicked};
-        match (is_over, prev, mouse.left.position) {
-            // LMB is down over the button. But the button wasn't Highlighted last
-            // update. This means the user clicked somewhere outside the button and
-            // moved over the button holding LMB down. We do nothing in this case.
-            (true,  Normal,  Down) => Normal,
-
-            // LMB is down over the button. The button was either Highlighted or Clicked
-            // last update. If it was highlighted before, that means the user clicked
-            // just now, and we transition to the Clicked state. If it was clicked
-            // before, that means the user is still holding LMB down from a previous
-            // click, in which case the state remains Clicked.
-            (true,  _,       Down) => Clicked,
-
-            // LMB is up. The mouse is hovering over the button. Regardless of what the
-            // state was last update, the state should definitely be Highlighted now.
-            (true,  _,       Up)   => Highlighted,
-
-            // LMB is down, the mouse is not over the button, but the previous state was
-            // Clicked. That means the user clicked the button and then moved the mouse
-            // outside the button while holding LMB down. The button stays Clicked.
-            (false, Clicked, Down) => Clicked,
-
-            // If none of the above applies, then nothing interesting is happening with
-            // this button.
-            _                      => Normal,
-        }
     }
 
     /// Return whether or not a given point is over a circle at a given point on a
@@ -194,8 +134,7 @@ mod circular_button {
     {
         /// The State struct that we defined above.
         type State = State;
-
-        /// The Style struct that we defined above.
+        /// The Style struct that we defined using the `widget_style!` macro.
         type Style = Style;
 
         fn common(&self) -> &CommonBuilder {
@@ -212,7 +151,6 @@ mod circular_button {
 
         fn init_state(&self) -> State {
             State {
-                interaction: Interaction::Normal,
                 circle_idx: IndexSlot::new(),
                 text_idx: IndexSlot::new(),
             }
@@ -222,76 +160,36 @@ mod circular_button {
             self.style.clone()
         }
 
-        /// Update the state of the button. The state may or may not have changed since
-        /// the last update. (E.g. it may have changed because the user moused over the
-        /// button.) If the state has changed, return the new state. Else, return None.
-        fn update<B: Backend>(mut self, args: UpdateArgs<Self, B>) {
+        /// Update the state of the button by handling any input that has occurred since the last
+        /// update.
+        fn update<B: Backend>(self, args: UpdateArgs<Self, B>) {
             let UpdateArgs { idx, state, rect, mut ui, style, .. } = args;
-            let (xy, dim) = rect.xy_dim();
-            let maybe_mouse = ui.input(idx).maybe_mouse.map(|mouse| mouse.relative_to(xy));
 
-            // Check whether or not a new interaction has occurred.
-            let new_interaction = match (self.enabled, maybe_mouse) {
-                (false, _) | (true, None) => Interaction::Normal,
-                (true, Some(mouse)) => {
-                    // Conrod does us a favor by transforming mouse.xy into this widget's
-                    // local coordinate system. Because mouse.xy is in local coords,
-                    // we must also pass the circle center in local coords. Thus we pass
-                    // [0.0, 0.0] as the center.
-                    //
-                    // See above where we define is_over_circ.
-                    let is_over = is_over_circ([0.0, 0.0], mouse.xy, dim);
+            let color = {
+                let input = ui.widget_input(idx);
 
-                    // See above where we define get_new_interaction.
-                    get_new_interaction(is_over, state.view().interaction, mouse)
-                },
-            };
-
-            // If the mouse was released over the button, react. state.interaction is the
-            // button's state as of a moment ago. new_interaction is the updated state as
-            // of right now. So this if statement is saying: If the button was clicked a
-            // moment ago, and it's now highlighted, then the button has been activated.
-            if let (Interaction::Clicked, Interaction::Highlighted) =
-                (state.view().interaction, new_interaction)
-            {
-                // Recall that our CircularButton struct includes maybe_react, which
-                // stores either a reaction function or None. If maybe_react is Some, call
-                // the function.
-                if let Some(ref mut react) = self.maybe_react {
-                    react();
+                // If the button was clicked, call the user's `react` function.
+                if input.clicks().left().next().is_some() {
+                    if let Some(mut react) = self.maybe_react {
+                        react();
+                    }
                 }
-            }
 
-            // Here we check to see whether or not our button should capture the mouse.
-            //
-            // Widgets can "capture" user input. If the button captures the mouse, then mouse
-            // events will only be seen by the button. Other widgets will not see mouse events
-            // until the button uncaptures the mouse.
-            match (state.view().interaction, new_interaction) {
-                // If the user has pressed the button we capture the mouse.
-                (Interaction::Highlighted, Interaction::Clicked) => {
-                    ui.capture_mouse(idx);
-                },
-                // If the user releases the button, we uncapture the mouse.
-                (Interaction::Clicked, Interaction::Highlighted) |
-                (Interaction::Clicked, Interaction::Normal)      => {
-                    ui.uncapture_mouse(idx);
-                },
-                _ => (),
-            }
-
-            // Whenever we call `state.update` (as below), a flag is set within our `State`
-            // indicating that there has been some mutation and that our widget requires a
-            // re-draw. Thus, we only want to call `state.update` if there has been some change in
-            // order to only re-draw when absolutely required.
-            //
-            // You can see how we do this below - we check if the state has changed before calling
-            // `state.update`.
-
-            // If the interaction has changed, set the new interaction.
-            if state.view().interaction != new_interaction {
-                state.update(|state| state.interaction = new_interaction);
-            }
+                let color = style.color(ui.theme());
+                input.mouse()
+                    .map(|mouse| {
+                        if is_over_circ([0.0, 0.0], mouse.rel_xy(), rect.dim()) {
+                            if mouse.buttons.left().is_down() {
+                                color.clicked()
+                            } else {
+                                color.highlighted()
+                            }
+                        } else {
+                            color
+                        }
+                    })
+                    .unwrap_or(color)
+            };
 
             // Finally, we'll describe how we want our widget drawn by simply instantiating the
             // necessary primitive graphics widgets.
@@ -309,7 +207,6 @@ mod circular_button {
 
             // First, we'll draw the **Circle** with a radius that is half our given width.
             let radius = rect.w() / 2.0;
-            let color = new_interaction.color(style.color(ui.theme()));
             let circle_idx = state.view().circle_idx.get(&mut ui);
             Circle::fill(radius)
                 .middle_of(idx)
@@ -333,43 +230,10 @@ mod circular_button {
 
     }
 
-    impl Style {
-        /// Construct the default Style.
-        pub fn new() -> Style {
-            Style {
-                maybe_color: None,
-                maybe_radius: None,
-                maybe_label_color: None,
-                maybe_label_font_size: None,
-            }
-        }
-
-        /// Get the Color for an Element.
-        pub fn color(&self, theme: &Theme) -> Color {
-            self.maybe_color.or(theme.widget_style::<Self>(KIND).map(|default| {
-                default.style.maybe_color.unwrap_or(theme.shape_color)
-            })).unwrap_or(theme.shape_color)
-        }
-
-        /// Get the label Color for an Element.
-        pub fn label_color(&self, theme: &Theme) -> Color {
-            self.maybe_label_color.or(theme.widget_style::<Self>(KIND).map(|default| {
-                default.style.maybe_label_color.unwrap_or(theme.label_color)
-            })).unwrap_or(theme.label_color)
-        }
-
-        /// Get the label font size for an Element.
-        pub fn label_font_size(&self, theme: &Theme) -> FontSize {
-            self.maybe_label_font_size.or(theme.widget_style::<Self>(KIND).map(|default| {
-                default.style.maybe_label_font_size.unwrap_or(theme.font_size_medium)
-            })).unwrap_or(theme.font_size_medium)
-        }
-    }
-
     /// Provide the chainable color() configuration method.
     impl<'a, F> Colorable for CircularButton<'a, F> {
         fn color(mut self, color: Color) -> Self {
-            self.style.maybe_color = Some(color);
+            self.style.color = Some(color);
             self
         }
     }
@@ -382,11 +246,11 @@ mod circular_button {
             self
         }
         fn label_color(mut self, color: Color) -> Self {
-            self.style.maybe_label_color = Some(color);
+            self.style.label_color = Some(color);
             self
         }
         fn label_font_size(mut self, size: FontSize) -> Self {
-            self.style.maybe_label_font_size = Some(size);
+            self.style.label_font_size = Some(size);
             self
         }
     }
