ea8acd03f3489ee13fc0b6a97eff209105d2be03
mitchmindtree <mitchell.nordine@gmail.com>
2016-07-30 18:17:12 +1000

    Complete the new example for the List widget

--- examples/list.rs
+++ examples/list.rs
@@ -4,64 +4,82 @@
 extern crate find_folder;
 extern crate piston_window;
 
-use conrod::{Theme, Widget};
 use piston_window::{EventLoop, OpenGL, PistonWindow, UpdateEvent, WindowSettings};
-
-
-/// Conrod is backend agnostic. Here, we define the `piston_window` backend to use for our `Ui`.
-type Backend = (piston_window::G2dTexture<'static>, piston_window::Glyphs);
-type Ui = conrod::Ui<Backend>;
-type UiCell<'a> = conrod::UiCell<'a, Backend>;
-
 
 fn main() {
 
     // Construct the window.
+    const WIDTH: u32 = 150;
+    const HEIGHT: u32 = 600;
     let mut window: PistonWindow =
-        WindowSettings::new("List Demo", [100, 600])
+        WindowSettings::new("List Demo", [WIDTH, HEIGHT])
             .opengl(OpenGL::V3_2).exit_on_esc(true).samples(4).vsync(true).build().unwrap();
-
-    // Construct our `Ui`.
-    let mut ui = {
-        let assets = find_folder::Search::KidsThenParents(3, 5)
-            .for_folder("assets").unwrap();
-        let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
-        let theme = Theme::default();
-        let glyph_cache = piston_window::Glyphs::new(&font_path, window.factory.clone()).unwrap();
-        Ui::new(glyph_cache, theme)
-    };
-
     window.set_ups(60);
 
-    let mut list = vec![true; 8];
+    // construct our `Ui`.
+    let mut ui = conrod::Ui::new(conrod::Theme::default());
+
+    // Add a `Font` to the `Ui`'s `font::Map` from file.
+    let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
+    let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
+    ui.fonts.insert_from_file(font_path).unwrap();
+
+    // No text to draw, so we'll just create an empty text texture cache.
+    let mut text_texture_cache =
+        conrod::backend::piston_window::GlyphCache::new(&mut window, WIDTH, HEIGHT);
+
+    // The image map describing each of our widget->image mappings (in our case, none).
+    let image_map = conrod::image::Map::new();
+
+    let mut list = vec![true; 16];
 
     // Poll events from the window.
     while let Some(event) = window.next() {
-        ui.handle_event(event.clone());
-        event.update(|_| ui.set_widgets(|ui_cell| set_ui(ui_cell, &mut list)));
-        window.draw_2d(&event, |c, g| ui.draw_if_changed(c, g));
+
+        // Convert the piston event to a conrod event.
+        if let Some(e) = conrod::backend::piston_window::convert_event(event.clone(), &window) {
+            ui.handle_event(e);
+        }
+
+        event.update(|_| {
+            ui.set_widgets(|ui_cell| set_ui(ui_cell, &mut list))
+        });
+
+        window.draw_2d(&event, |c, g| {
+            if let Some(primitives) = ui.draw_if_changed(&image_map) {
+                fn texture_from_image<T>(img: &T) -> &T { img };
+                conrod::backend::piston_window::draw(c, g, primitives,
+                                                     &mut text_texture_cache,
+                                                     texture_from_image);
+            }
+        });
     }
 
 }
 
 // Declare the `WidgetId`s and instantiate the widgets.
-fn set_ui(ref mut ui: UiCell, list: &mut [bool]) {
-    use conrod::{Canvas, color, Colorable, List, Oval, Positionable, Sizeable};
+fn set_ui(ref mut ui: conrod::UiCell, list: &mut [bool]) {
+    use conrod::{Colorable, Labelable, Positionable, Sizeable, Widget};
 
     widget_ids!{CANVAS, LIST};
 
-    Canvas::new().color(color::DARK_CHARCOAL).set(CANVAS, ui);
+    conrod::Canvas::new().color(conrod::color::DARK_CHARCOAL).set(CANVAS, ui);
 
-    const ITEM_HEIGHT: conrod::Scalar = 25.0;
+    const ITEM_HEIGHT: conrod::Scalar = 50.0;
 
-    List::new(list.len(), ITEM_HEIGHT)
-        .color(color::LIGHT_BLUE)
-        .scroll_kids_vertically()
+    conrod::List::new(list.len() as u32, ITEM_HEIGHT)
+        .scrollbar_on_top()
+        .middle_of(CANVAS)
+        .wh_of(CANVAS)
         .item(|item| {
-
-            item.layout(Toggle::new(list[item.list_idx]))
-                .set(item.widget_idx, &mut ui);
-
+            let i = item.i;
+            let label = format!("item {}: {}", i, list[i]);
+            let toggle = conrod::Toggle::new(list[i])
+                .label(&label)
+                .label_color(conrod::color::WHITE)
+                .color(conrod::color::LIGHT_BLUE)
+                .react(|v| list[i] = v);
+            item.set(toggle);
         })
         .set(LIST, ui);
 }
