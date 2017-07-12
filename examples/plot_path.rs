e79fc06fe6e4e43e815d66fd14feb304b9e0682f
mitchmindtree <mitchell.nordine@gmail.com>
2017-07-13 01:11:06 +1000

    Re-add grid to plot

--- examples/plot_path.rs
+++ examples/plot_path.rs
@@ -15,7 +15,7 @@ mod feature {
     use support;
 
     widget_ids! {
-        struct Ids { canvas, plot }
+        struct Ids { canvas, grid, plot }
     }
 
     pub fn main() {
@@ -86,8 +86,24 @@ mod feature {
                 let max_x = std::f64::consts::PI * 2.0;
                 let min_y = -1.0;
                 let max_y = 1.0;
+
+                let quarter_lines = widget::grid::Lines::step(0.5_f64).thickness(2.0);
+                let sixteenth_lines = widget::grid::Lines::step(0.125_f64).thickness(1.0);
+                let lines = &[
+                    quarter_lines.x(),
+                    quarter_lines.y(),
+                    sixteenth_lines.x(),
+                    sixteenth_lines.y(),
+                ];
+
+                widget::Grid::new(min_x, max_x, min_y, max_y, lines.iter().cloned())
+                    .color(color::rgb(0.1, 0.12, 0.15))
+                    .wh_of(ids.canvas)
+                    .middle_of(ids.canvas)
+                    .set(ids.grid, ui);
                 widget::PlotPath::new(min_x, max_x, min_y, max_y, f64::sin)
                     .color(color::LIGHT_BLUE)
+                    .thickness(2.0)
                     .wh_of(ids.canvas)
                     .middle_of(ids.canvas)
                     .set(ids.plot, ui);
