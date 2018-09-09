fdc54887184cd1fcbfd8a5a95d26c2196a193a3b
bobgates <dvogt@polka.co.za>
2018-09-09 21:17:44 +0100

    fixed test errors in doc by use of `txt

--- src/guide/chapter_3.rs
+++ src/guide/chapter_3.rs
@@ -1,4 +1,11 @@
 /*!
+# #[macro_use]
+# extern crate conrod;
+# extern crate find_folder;
+
+# use conrod::{self, widget, Positionable, Colorable, Widget};
+# use conrod::backend::glium::glium::{self, Surface};
+
 
 **Setting up a basic window using glium and winit**
 
@@ -14,23 +21,25 @@ example on a mac: /Users/bob/Projects, or perhaps D:\Projects on a PC, and use
 cargo to create a new project. I've called the project conrod_hw for hello
 world, you can use any name. Type at the command line:
 
-```txt cargo new conrod_hw --bin
+```txt
+cargo new conrod_hw --bin
 ```
 `--bin` tells cargo to build an executable project.
 
  If you now go into the project and run it with:
 
-```
+```txt
 cd conrod_hw cargo run
 ```
 
 you'll probably see a fairly long and slow list of rust files being downloaded
 and compiled, then something like:
 
-```txt Compiling conrod_hw v0.1.0
-   (file:///Users/declan/Projects/rust/conrod_hw) 
-   Finished dev [unoptimized + debuginfo] target(s) in 0.34 secs 
-   Running `target/debug/conrod_hw` 
+```txt
+   Compiling conrod_hw v0.1.0
+   (file:///Users/declan/Projects/rust/conrod_hw)
+   Finished dev [unoptimized + debuginfo] target(s) in 0.34 secs
+   Running `target/debug/conrod_hw`
    Hello, world!
 ```
 If you don't see these, you've got a problem with your Rust installation.
@@ -40,28 +49,33 @@ Cargo builds a default "Hello World" command line app when you ask it to
 create a new --bin project. In future, most of the crates that you've just
 compiled won't need recompilation, and things will happen more quickly.
 
-## Setting up Cargo for Conrod 
+## Setting up Cargo for Conrod
 
 To use Conrod in your application, you need to tell your program to use it,
 and you need to tell Cargo to make it available to your programme. The program
 itself is in the `src` folder of your project, in the file called `main.rs`,
 and should look like this right now:
 
-```ignore fn main() { println!("Hello, world!"); }
+``` fn main() {
+    println!("Hello, world!");
+}
 ```
 
 To allow your programme to use conrod, you need to add a reference to the
 conrod crate, with:
 
-```ignore 
+```ignore
 #[macro_use] extern crate conrod;
 
-fn main() { println!("Hello, world!"); }
+fn main() {
+    println!("Hello, world!");
+}
 ```
 
 If you try to run the above code, you'll get an error like:
 
-```ignore error[E0463]: can't find crate for "conrod"
+```txt
+error[E0463]: can't find crate for "conrod"
 
  --> src/main.rs:1:1
 
@@ -73,10 +87,10 @@ It needs to be added to the manifest for the project, in the  `cargo.toml`
 file, which is in the root of the project. The file currently contains
 something like:
 
-```ignore 
-[package] 
-name = "conrod_hw" 
-version = "0.1.0" 
+```toml
+[package]
+name = "conrod_hw"
+version = "0.1.0"
 authors = ["bobgates"]
 
 [dependencies]
@@ -88,7 +102,7 @@ Add `conrod = { version = "0.55.0", features = ["glium", "winit"] }` to the
 This incantation needs a little explanation, because it specifies the
 backends.
 
-### Backends 
+### Backends
 
 Conrod is designed as an IMGUI that can use a number of backends. It was
 initially developed to support PistonWindow, but then had backends added for
@@ -99,31 +113,34 @@ specifying the backend in `cargo.toml` because we then won't have to think
 about it in the code itself. Alternatively, the application can be run using
 cargo "features" as described in the previous chapter.
 
-```txt cargo run --release --features "winit glium" --example all_winit_glium
+```txt
+    cargo run --release --features "winit glium" --example all_winit_glium
 ```
 
 At the time of writing (September 2017), the winit and glium backend
 combination seems to be the best supported, and its the one that I am familiar
 with.
 
-When the program compiles, we're ready to build a UI. (there will be  a
+When the program compiles, we're ready to build a UI (there will be  a
 warning about macro use, but we'll be using macros soon).
 
-## Creating a Window 
+## Creating a Window
 
 There is quite a lot to do for even an empty window, so let's get to it.
 
 I'm using the glium backend. [Glium](https://github.com/glium/glium) is a
 cross-platform, safe wrapper for OpenGL, written in Rust. It is listed as not-
 maintained on Github, but limited maintenance is being undertaken by the
-writer of Conrod. Glium has a very good, gentle
+developer of Conrod. Glium has a very good, gentle
 [introduction](http://glium.github.io/glium/book/) to OpenGL - enough to
 convince me that I really want Conrod to do the work for me!
 
 Conrod makes Glium available through an export. Put the following line into
 the code, near the top, to include it:
 
+```txt
     use conrod::backend::glium::glium::{self, Surface};
+```
 
 `Surface` is a trait required for glium, specifically for the call to
 `target.clear_color` which is coming later.
@@ -133,16 +150,16 @@ interaction with the UI, then a window, then a context, then finally links the
 event loop, window and context together into a display. The display is the
 home for the UI, and is an OpenGL context provided by glium.
 
-```
-const WIDTH: u32 = 400; 
+```txt
+const WIDTH: u32 = 400;
 const HEIGHT: u32 = 200;
 
-let mut events_loop = glium::glutin::EventsLoop::new(); 
-let window = glium::glutin::WindowBuilder::new() 
+let mut events_loop = glium::glutin::EventsLoop::new();
+let window = glium::glutin::WindowBuilder::new()
                 .with_title("Hello Conrod")
-                .with_dimensions(WIDTH, HEIGHT); 
-let context = glium::glutin::ContextBuilder::new() 
-                .with_vsync(true) 
+                .with_dimensions(WIDTH, HEIGHT);
+let context = glium::glutin::ContextBuilder::new()
+                .with_vsync(true)
                 .with_multisampling(4);
 let display = glium::Display::new(window, context, &events_loop).unwrap();
 ```
@@ -150,33 +167,38 @@ let display = glium::Display::new(window, context, &events_loop).unwrap();
 Now create the UI itself. Conrod has a builder that contains and looks after
 the UI for the user.
 
+```txt
     let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+```
 
 
 Conrod can use graphics. It stores these in a map. The system needs the map,
 even though it doesn't contain anything at this time, so create it:
 
+```txt
     let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+```
 
 Finally, Conrod needs to render its UI. It uses a renderer to do this, so
 create one:
 
+```txt
     let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
-
+```
 
 As an Immediate Mode GUI, Conrod sits in the main loop of the program, drawing
 the UI every time round the loop. Here's the main loop:
 
-```
-'main: loop { 
-    // Render the `Ui` and then display it on the screen. 
-    if let Some(primitives) = ui.draw_if_changed() { 
-        renderer.fill(&display, primitives, &image_map); 
+```txt
+'main: loop {
+    // Render the `Ui` and then display it on the screen.
+    if let Some(primitives) = ui.draw_if_changed() {
+        renderer.fill(&display, primitives, &image_map);
         let mut target = display.draw();
-        target.clear_color(0.0, 1.0, 0.0, 1.0); 
-        renderer.draw(&display, &mut target, &image_map).unwrap(); 
-        target.finish().unwrap(); 
-    } 
+        target.clear_color(0.0, 1.0, 0.0, 1.0);
+        renderer.draw(&display, &mut target, &image_map).unwrap();
+        target.finish().unwrap();
+    }
 }
 ```
 
@@ -203,13 +225,16 @@ ever - a green block with a white "menu bar" that has nothing in it. Let's add
 at least something to see.
 
 After
-
+# const WIDTH: i32 = 400;
+# const HEIGHT: i32 = 640;
+```txt
     let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+```
 
 add the code that will initiate and build the UI:
 
-```
-widget_ids!(struct Ids { text }); 
+```txt
+widget_ids!(struct Ids { text });
 let ids = Ids::new(ui.widget_id_generator());
 ```
 
@@ -224,15 +249,15 @@ After defining the widget_ids, `Ids::new` creates the the widget structure.
 To instantiate the widgets, insert the following code in the main loop, before
 the rendering statement.
 
-```
+```txt
 let ui = &mut ui.set_widgets();
 
-// "Hello World!" in the middle of the screen. 
-widget::Text::new("Hello World!") 
-    .middle_of(ui.window) 
-    .color(conrod::color::WHITE) 
+// "Hello World!" in the middle of the screen.
+widget::Text::new("Hello World!")
+    .middle_of(ui.window)
+    .color(conrod::color::WHITE)
     .font_size(32)
-    .set(ids.text, ui); 
+    .set(ids.text, ui);
 ```
 
 In the definition of the Ids struct above, only one widget was named: `text`.
@@ -265,13 +290,14 @@ trait of type 'Colorable'. To ensure that the program can find the trait, we
 need to add to the `use conrod` line, as suggested in the error message. We
 also need to define `widget' and the type of Widget, so add the following near
 the top of the programme:
-
+```txt
     use conrod::{widget, Positionable, Colorable, Widget};
+```
 
 There's one more thing we need to do. Conrod needs to load fonts before it can
 use them. Some boilerplate in the early part of the programme can do this:
 
-```
+```txt
 let assets = find_folder::Search::KidsThenParents(3,
 5).for_folder("assets").unwrap(); let font_path = assets.join("fonts/NotoSans
 /NotoSans-Regular.ttf"); ui.fonts.insert_from_file(font_path).unwrap();
@@ -281,15 +307,16 @@ The code uses the find_folder crate. Modify the Cargo.toml file for this
 project by adding find_folder to the dependencies. Its current version at time
 of writing is 0.3.0:
 
-```
-[dependencies] 
+```txt
+[dependencies]
 conrod = { version = "0.55.0", features = ["glium", "winit"] }
 find_folder="0.3.0"
 ```
 
 and reference the crate at the beginning of the file:
-
+```txt
     extern crate find_folder;
+```
 
 `find_folder` and some Rust path handling generates the path to the font. It
 is in the folder `assets/fonts/NotoSans` and is called `NotoSans-Regular.ttf`.
@@ -307,32 +334,32 @@ visible on this white web page):
 
 Not quite what we want, yet.
 
-## Event Handling 
+## Event Handling
 
 The window is not getting all its furniture because its events are not being
 handled. At its most simple, events can be handled by fetching them from the
 `event_loop` and dispatching them accordingly. Here's the code in the main
 loop:
 
-```
-let mut events = Vec::new(); 
+```txt
+let mut events = Vec::new();
 events_loop.poll_events(|event| events.push(event));
 
-for event in events{ 
+for event in events{
     match event {
         glium::glutin::Event::WindowEvent { event, ..} => match event {
             glium::glutin::WindowEvent::Closed |
-            glium::glutin::WindowEvent::KeyboardInput { 
-                input: glium::glutin::KeyboardInput { 
+            glium::glutin::WindowEvent::KeyboardInput {
+                input: glium::glutin::KeyboardInput {
                     virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
                     ..
                 },
                 ..
-            } => break 'main, 
-            _ => (), 
-        }, 
-        _ => (), 
-    } 
+            } => break 'main,
+            _ => (),
+        },
+        _ => (),
+    }
 }
 ```
 
@@ -357,7 +384,7 @@ respond to any of its own events, so they're not taken into account.
 First, let's add event handling for Conrod. Putting the following at the
 beginning of the event for loop will take care of UI events:
 
-```
+```txt
     if let Some(event) = conrod::backend::winit::convert_event(event.clone(),
         &display) { ui.handle_event(event); }
 ```
@@ -371,57 +398,57 @@ Then there's some straightforward event boilerplate that most Conrod apps have
 somewhere convenient. In this case, I'm going to put it into the same file as
 `fn main()'.
 
-```
-pub struct EventLoop { 
-        ui_needs_update: bool, 
+```txt
+pub struct EventLoop {
+        ui_needs_update: bool,
         last_update: std::time::Instant,
     }
 
 impl EventLoop {
 
-    pub fn new() -> Self { 
+    pub fn new() -> Self {
         EventLoop { last_update: std::time::Instant::now(),
-                    ui_needs_update: true, 
-                  } 
+                    ui_needs_update: true,
+                  }
     }
 
-    /// Produce an iterator yielding all available events. 
+    /// Produce an iterator yielding all available events.
     pub fn next(&mut self, events_loop: &mut glium::glutin::EventsLoop) ->
-                Vec<glium::glutin::Event> { 
+                Vec<glium::glutin::Event> {
 
-        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms 
-        // since the last yield. 
-        let last_update = self.last_update; 
-        let sixteen_ms = std::time::Duration::from_millis(16); 
-        let duration_since_last_update = std::time::Instant::now().duration_since(last_update); 
-        if duration_since_last_update < sixteen_ms { 
-            std::thread::sleep(sixteen_ms - duration_since_last_update); 
+        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
+        // since the last yield.
+        let last_update = self.last_update;
+        let sixteen_ms = std::time::Duration::from_millis(16);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        if duration_since_last_update < sixteen_ms {
+            std::thread::sleep(sixteen_ms - duration_since_last_update);
         }
 
-        // Collect all pending events. 
+        // Collect all pending events.
         let mut events = Vec::new();
         events_loop.poll_events(|event| events.push(event));
 
-        // If there are no events and the UI does not need updating, wait 
-        // for the next event. 
+        // If there are no events and the UI does not need updating, wait
+        // for the next event.
         if events.is_empty() && !self.ui_needs_update {
             events_loop.run_forever(|event| { events.push(event);
-                                    glium::glutin::ControlFlow::Break }); 
+                                    glium::glutin::ControlFlow::Break });
         }
 
-        self.ui_needs_update = false; 
+        self.ui_needs_update = false;
         self.last_update = std::time::Instant::now();
 
-        events 
+        events
     }
 
     /// Notifies the event loop that the `Ui` requires another update whether
-    /// or not there are any pending events. 
-    /// 
-    /// This is primarily used on the occasion that some part of the UI is 
-    /// still animating and requires further updates to do so. 
+    /// or not there are any pending events.
+    ///
+    /// This is primarily used on the occasion that some part of the UI is
+    /// still animating and requires further updates to do so.
     pub fn needs_update(&mut self) {
-        self.ui_needs_update = true; 
+        self.ui_needs_update = true;
     }
 ```
 
@@ -435,20 +462,21 @@ update.
 The main events loop can now be changed. Insert a constructor for the event
 loop just before the 'main loop
 
+```txt
      let mut event_loop = EventLoop::new();
-
+```
 
 Remove the quick-and-dirty event queue introduced above
 
-```
-let mut events = Vec::new(); 
+```txt
+let mut events = Vec::new();
 events_loop.poll_events(|event|events.push(event));
 ```
 
 and change the event `for` loop to read from the `event_loop` that was
 constructed above.
 
-```
+```txt
 for event in event_loop.next(&mut events_loop){
 ```
 
