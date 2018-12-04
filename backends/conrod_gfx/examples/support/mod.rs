d07df2e8121cb283b1821f7f84e732b900eae6d7
mitchmindtree <mitchell.nordine@gmail.com>
2018-12-04 14:22:25 +0700

    Remove backend suffix and unnecessary feature from conrod_gfx

--- backends/conrod_gfx/examples/support/mod.rs
+++ backends/conrod_gfx/examples/support/mod.rs
@@ -29,7 +29,6 @@ pub struct DemoApp {
 
 
 impl DemoApp {
-
     /// Simple constructor for the `DemoApp`.
     pub fn new(rust_logo: conrod_core::image::Id) -> Self {
         DemoApp {
@@ -39,7 +38,6 @@ impl DemoApp {
             rust_logo: rust_logo,
         }
     }
-
 }
 
 /// A set of reasonable stylistic defaults that works for the `gui` below.
@@ -65,11 +63,9 @@ pub fn theme() -> conrod_core::Theme {
     }
 }
 
-
 // Generate a unique `WidgetId` for each widget.
 widget_ids! {
     pub struct Ids {
-
         // The scrollable canvas.
         canvas,
 
@@ -110,7 +106,6 @@ widget_ids! {
 
         // Scrollbar
         canvas_scrollbar,
-
     }
 }
 
@@ -139,7 +134,7 @@ pub fn gui(ui: &mut conrod_core::UiCell, ids: &Ids, app: &mut DemoApp) {
 
     // We'll demonstrate the `Text` primitive widget by using it to draw a title and an
     // introduction to the example.
-    //widget::Text::new(TITLE).font_size(TITLE_SIZE).mid_top_of(ids.canvas).set(ids.title, ui);
+    widget::Text::new(TITLE).font_size(TITLE_SIZE).mid_top_of(ids.canvas).set(ids.title, ui);
 
     const INTRODUCTION: &'static str =
         "This example aims to demonstrate all widgets that are provided by conrod.\
@@ -348,68 +343,4 @@ pub fn gui(ui: &mut conrod_core::UiCell, ids: &Ids, app: &mut DemoApp) {
 
 
     widget::Scrollbar::y_axis(ids.canvas).auto_hide(true).set(ids.canvas_scrollbar, ui);
-
-}
-
-
-/// In most of the examples the `glutin` crate is used for providing the window context and
-/// events while the `glium` crate is used for displaying `conrod_core::render::Primitives` to the
-/// screen.
-/// 
-/// This `Iterator`-like type simplifies some of the boilerplate involved in setting up a
-/// glutin+glium event loop that works efficiently with conrod.
-#[cfg(feature="glium")]
-pub struct EventLoop {
-    ui_needs_update: bool,
-    last_update: std::time::Instant,
-}
-
-#[cfg(feature="glium")]
-impl EventLoop {
-
-    pub fn new() -> Self {
-        EventLoop {
-            last_update: std::time::Instant::now(),
-            ui_needs_update: true,
-        }
-    }
-
-    /// Produce an iterator yielding all available events.
-    pub fn next(&mut self, events_loop: &mut glium::glutin::EventsLoop) -> Vec<glium::glutin::Event> {
-        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
-        // since the last yield.
-        let last_update = self.last_update;
-        let sixteen_ms = std::time::Duration::from_millis(16);
-        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
-        if duration_since_last_update < sixteen_ms {
-            std::thread::sleep(sixteen_ms - duration_since_last_update);
-        }
-
-        // Collect all pending events.
-        let mut events = Vec::new();
-        events_loop.poll_events(|event| events.push(event));
-
-        // If there are no events and the `Ui` does not need updating, wait for the next event.
-        if events.is_empty() && !self.ui_needs_update {
-            events_loop.run_forever(|event| {
-                events.push(event);
-                glium::glutin::ControlFlow::Break
-            });
-        }
-
-        self.ui_needs_update = false;
-        self.last_update = std::time::Instant::now();
-
-        events
-    }
-
-    /// Notifies the event loop that the `Ui` requires another update whether or not there are any
-    /// pending events.
-    ///
-    /// This is primarily used on the occasion that some part of the `Ui` is still animating and
-    /// requires further updates to do so.
-    pub fn needs_update(&mut self) {
-        self.ui_needs_update = true;
-    }
-
 }
