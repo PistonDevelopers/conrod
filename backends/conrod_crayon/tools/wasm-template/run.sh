ddd91be7b19d37337197284c403e6e4e9fb94144
alanpoon <brightshadowx@gmail.com>
2019-03-10 22:21:22 +0800

    t3

--- backends/conrod_crayon/tools/wasm-template/run.sh
+++ backends/conrod_crayon/tools/wasm-template/run.sh
@@ -3,6 +3,8 @@ BASE_DIR=$SCRIPT_DIR/../../../../
 
 set -e
 cargo build --example $1 --target wasm32-unknown-unknown
+mkdir $SCRIPT_DIR/dist
+mkdir $SCRIPT_DIR/dist/intermediate
 cp $BASE_DIR/target/wasm32-unknown-unknown/debug/examples/$1.wasm $SCRIPT_DIR/dist/intermediate/native.wasm
 wasm-bindgen $SCRIPT_DIR/dist/intermediate/native.wasm --out-dir $SCRIPT_DIR/dist
 
