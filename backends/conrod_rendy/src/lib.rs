a0ab26e1fbdf892fbf5d994df7f274490a21c4a6
David Partouche <david@kaligs.com>
2019-12-14 19:05:00 +0800

    Adding image support (currently crashes)

--- backends/conrod_rendy/src/lib.rs
+++ backends/conrod_rendy/src/lib.rs
@@ -1,14 +1,17 @@
+pub mod winit_convert;
+
 use conrod_core::render::{Primitive, PrimitiveKind, PrimitiveWalker};
 use conrod_core::{color, Scalar, Ui};
 use gfx_backend_vulkan;
 use rendy::command::{Families, QueueId, RenderPassEncoder};
 use rendy::core::{hal, hal::pso::CreationError, types::Layout};
-use rendy::factory::{Config, Factory};
+use rendy::factory::{Config, Factory, ImageState};
 use rendy::graph::{
     present::PresentNode, render::*, Graph, GraphBuilder, GraphContext, NodeBuffer, NodeImage,
 };
 use rendy::hal::{
     command::{ClearColor, ClearValue},
+    device::Device,
     format::Format,
     image::Kind,
     pso::{DepthStencilDesc, Element, VertexInputRate},
@@ -22,13 +25,14 @@ use rendy::init::{
     WindowedRendy,
 };
 use rendy::memory::Dynamic;
-use rendy::resource::{Buffer, BufferInfo, DescriptorSetLayout, Escape, Handle};
+use rendy::resource::{Buffer, BufferInfo, DescriptorSet, DescriptorSetLayout, Escape, Handle};
 use rendy::shader::{
     ShaderKind, ShaderSet, ShaderSetBuilder, SourceLanguage, SourceShaderInfo, SpirvReflection,
     SpirvShader,
 };
-
-pub mod winit_convert;
+use rendy::texture::{image::ImageTextureConfig, Texture};
+use std::fs::File;
+use std::io::BufReader;
 
 /// Draw text from the text cache texture `tex` in the fragment shader.
 pub const MODE_TEXT: u32 = 0;
@@ -67,6 +71,7 @@ void main() {
     static ref FRAGMENT: SpirvShader = SourceShaderInfo::new(
     "
 #version 450
+layout(set = 0, binding = 0) uniform sampler2D t_Color;
 
 layout(location = 0) in vec2 v_Uv;
 layout(location = 1) in vec4 v_Color;
@@ -77,11 +82,11 @@ layout(location = 0) out vec4 Target0;
 void main() {
     // Text
     if (v_Mode == uint(0)) {
-        Target0 = v_Color;
+        Target0 = v_Color * vec4(1.0, 1.0, 1.0, texture(t_Color, v_Uv).r);
 
     // Image
     } else if (v_Mode == uint(1)) {
-        Target0 = v_Color;
+        Target0 = texture(t_Color, v_Uv);
 
     // 2D Geometry
     } else if (v_Mode == uint(2)) {
@@ -116,7 +121,9 @@ pub struct ConrodPipelineDesc;
 
 #[derive(Debug)]
 pub struct ConrodPipeline<B: Backend> {
+    descriptor_set: Escape<DescriptorSet<B>>,
     buffer: Option<Escape<Buffer<B>>>,
+    texture: Texture<B>,
     vertice_count: u32,
 }
 
@@ -148,19 +155,65 @@ where
     fn build<'a>(
         self,
         _ctx: &GraphContext<B>,
-        _factory: &mut Factory<B>,
-        _queue: QueueId,
+        factory: &mut Factory<B>,
+        queue: QueueId,
         _ui: &Ui,
-        buffers: Vec<NodeBuffer>,
-        images: Vec<NodeImage>,
+        _buffers: Vec<NodeBuffer>,
+        _images: Vec<NodeImage>,
         set_layouts: &[Handle<DescriptorSetLayout<B>>],
     ) -> Result<Self::Pipeline, CreationError> {
-        assert!(buffers.is_empty());
-        assert!(images.is_empty());
-        assert!(set_layouts.is_empty());
+        // This is how we can load an image and create a new texture.
+        let assets = find_folder::Search::KidsThenParents(3, 5)
+            .for_folder("assets")
+            .unwrap();
+        let logo_path = assets.join("images/rust.png");
+        let image_reader =
+            BufReader::new(File::open(logo_path).map_err(|_| hal::pso::CreationError::Other)?);
+
+        let texture_builder = rendy::texture::image::load_from_image(
+            image_reader,
+            ImageTextureConfig {
+                generate_mips: true,
+                ..Default::default()
+            },
+        )
+        .map_err(|_| hal::pso::CreationError::Other)?;
+
+        let texture = texture_builder
+            .build(
+                ImageState {
+                    queue,
+                    stage: hal::pso::PipelineStage::FRAGMENT_SHADER,
+                    access: hal::image::Access::SHADER_READ,
+                    layout: hal::image::Layout::ShaderReadOnlyOptimal,
+                },
+                factory,
+            )
+            .unwrap();
+
+        let descriptor_set = factory
+            .create_descriptor_set(set_layouts[0].clone())
+            .unwrap();
+
+        unsafe {
+            factory
+                .device()
+                .write_descriptor_sets(vec![hal::pso::DescriptorSetWrite {
+                    set: descriptor_set.raw(),
+                    binding: 0,
+                    array_offset: 0,
+                    descriptors: vec![hal::pso::Descriptor::CombinedImageSampler(
+                        texture.view().raw(),
+                        hal::image::Layout::ShaderReadOnlyOptimal,
+                        texture.sampler().raw(),
+                    )],
+                }]);
+        }
 
         Ok(ConrodPipeline {
+            descriptor_set,
             buffer: None,
+            texture,
             vertice_count: 0,
         })
     }
@@ -216,13 +269,19 @@ where
 
     fn draw(
         &mut self,
-        _layout: &<B as Backend>::PipelineLayout,
+        layout: &<B as Backend>::PipelineLayout,
         mut encoder: RenderPassEncoder<'_, B>,
         _index: usize,
         _aux: &Ui,
     ) {
         if let Some(buffer) = &self.buffer {
             unsafe {
+                encoder.bind_graphics_descriptor_sets(
+                    layout,
+                    0,
+                    std::iter::once(self.descriptor_set.raw()),
+                    std::iter::empty::<u32>(),
+                );
                 encoder.bind_vertex_buffers(0, Some((buffer.raw(), 0)));
                 encoder.draw(0..self.vertice_count, 0..1);
             }
@@ -234,18 +293,19 @@ where
 
 impl<B: Backend> ConrodPipeline<B> {
     fn fill(&self, ui: &Ui, primitive: Primitive) -> Vec<Vertex> {
+        // TODO: Get the dpi factor
+        let dpi_factor = 1.0;
+        let half_win_w = ui.win_w / 2.0;
+        let half_win_h = ui.win_h / 2.0;
+
         // Functions for converting for conrod scalar coords to GL vertex coords (-1.0 to 1.0).
-        let vx = |x: Scalar| (x / (ui.win_w / 2.0)) as f32;
-        let vy = |y: Scalar| -1.0 * (y / (ui.win_h / 2.0)) as f32;
+        let vx = |x: Scalar| (x * dpi_factor / half_win_w) as f32;
+        let vy = |y: Scalar| -1.0 * (y * dpi_factor / half_win_h) as f32;
 
         let mut vertices = vec![];
 
         match primitive.kind {
-            PrimitiveKind::Rectangle { color }
-            | PrimitiveKind::Text { color, .. }
-            | PrimitiveKind::Image {
-                color: Some(color), ..
-            } => {
+            PrimitiveKind::Rectangle { color } => {
                 let color = color.to_fsa();
                 let (l, r, b, t) = primitive.rect.l_r_b_t();
 
@@ -298,6 +358,55 @@ impl<B: Backend> ConrodPipeline<B> {
                     vertices.push(v(triangle[1]));
                     vertices.push(v(triangle[2]));
                 }
+            }
+            PrimitiveKind::Image {
+                color, source_rect, ..
+            } => {
+                let color = color.unwrap_or(color::WHITE).to_fsa();
+                // TODO: Get the image reference
+                //                let (image_w, image_h) = (image_ref.width, image_ref.height);
+                let (image_w, image_h) = (144, 144);
+                let (image_w, image_h) = (image_w as Scalar, image_h as Scalar);
+
+                let (uv_l, uv_r, uv_b, uv_t) = match source_rect {
+                    Some(src_rect) => {
+                        let (l, r, b, t) = src_rect.l_r_b_t();
+                        (
+                            (l / image_w) as f32,
+                            (r / image_w) as f32,
+                            1.0 - (b / image_h) as f32,
+                            1.0 - (t / image_h) as f32,
+                        )
+                    }
+                    None => (0.0, 1.0, 1.0, 0.0),
+                };
+
+                let v = |x, y, t| {
+                    // Convert from conrod Scalar range to GL range -1.0 to 1.0.
+                    let x = (x * dpi_factor / half_win_w) as f32;
+                    let y = -((y * dpi_factor / half_win_h) as f32);
+                    Vertex {
+                        pos: [x, y],
+                        uv: t,
+                        color: color,
+                        mode: MODE_IMAGE,
+                    }
+                };
+
+                let mut push_v = |x, y, t| vertices.push(v(x, y, t));
+
+                // Swap bottom and top to suit reversed vulkan coords.
+                let (l, r, b, t) = primitive.rect.l_r_b_t();
+
+                // Bottom left triangle.
+                push_v(l, t, [uv_l, uv_t]);
+                push_v(r, b, [uv_r, uv_b]);
+                push_v(l, b, [uv_l, uv_b]);
+
+                // Top right triangle.
+                push_v(l, t, [uv_l, uv_t]);
+                push_v(r, b, [uv_r, uv_b]);
+                push_v(r, t, [uv_r, uv_t]);
             }
             _ => {}
         }
@@ -364,6 +473,7 @@ impl Renderer {
     }
 
     pub fn draw(&mut self, ui: &Ui) {
+        self.factory.maintain(&mut self.families);
         if let Some(graph) = self.graph.as_mut() {
             graph.run(&mut self.factory, &mut self.families, &ui);
         }
