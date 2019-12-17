8912b8df9224e4d9625d75c8689116c2f9cedbdc
David Partouche <david@kaligs.com>
2019-12-17 13:46:27 +0800

    Adding generic backend for rendy

--- backends/conrod_rendy/examples/all_winit_rendy.rs
+++ backends/conrod_rendy/examples/all_winit_rendy.rs
@@ -1,25 +1,25 @@
 use conrod_example_shared::{WIN_H, WIN_W};
-use conrod_rendy::{ConrodAux, Image, ConrodPipeline};
+use conrod_rendy::{ConrodAux, ConrodPipeline, Image};
 use rendy::{
-    command::{Families},
+    command::Families,
     factory::{self, Factory},
     graph::{
-        Graph,
-        GraphBuilder,
         present::PresentNode,
         render::{RenderGroupBuilder, SimpleGraphicsPipeline},
+        Graph, GraphBuilder,
     },
     hal::{
         command::{ClearColor, ClearValue},
         image::Kind,
+        Backend,
     },
     init::{
-        WindowedRendy,
         winit::{
             event::{Event, WindowEvent},
             event_loop::{ControlFlow, EventLoop},
             window::{Window, WindowBuilder},
         },
+        AnyWindowedRendy,
     },
 };
 
@@ -60,13 +60,10 @@ fn main() {
     };
 
     let config: factory::Config = Default::default();
-    let rendy = WindowedRendy::init(&config, window_builder, &event_loop).unwrap();
-    let mut factory = rendy.factory;
-    let mut families = rendy.families;
-    let surface = rendy.surface;
-    let window = rendy.window;
-
-    let mut graph_builder = GraphBuilder::<gfx_backend_vulkan::Backend, ConrodAux>::new();
+    let rendy = AnyWindowedRendy::init_auto(&config, window_builder, &event_loop).unwrap();
+    rendy::with_any_windowed_rendy!((rendy)
+        (mut factory, mut families, surface, window) => {
+    let mut graph_builder = GraphBuilder::<_, ConrodAux>::new();
     let size = window
         .inner_size()
         .to_physical(window.hidpi_factor());
@@ -74,8 +71,7 @@ fn main() {
     let color = graph_builder.create_image(
         Kind::D2(size.width as u32, size.height as u32, 1, 1),
         1,
-        (&factory as &Factory<gfx_backend_vulkan::Backend>)
-            .get_surface_format(&surface),
+        factory.get_surface_format(&surface),
         Some(ClearValue {
             color: ClearColor {
                 float32: CLEAR_COLOR,
@@ -99,22 +95,21 @@ fn main() {
         .unwrap();
 
     run(event_loop, aux, ids, app, factory, families, window, Some(graph));
+    });
 }
 
-pub fn run(
+pub fn run<B: Backend>(
     event_loop: EventLoop<()>,
     mut aux: ConrodAux,
     ids: conrod_example_shared::Ids,
     mut app: conrod_example_shared::DemoApp,
-    mut factory: Factory<gfx_backend_vulkan::Backend>,
-    mut families: Families<gfx_backend_vulkan::Backend>,
+    mut factory: Factory<B>,
+    mut families: Families<B>,
     window: Window,
-    mut graph: Option<Graph<gfx_backend_vulkan::Backend, ConrodAux>>,
+    mut graph: Option<Graph<B, ConrodAux>>,
 ) {
     event_loop.run(move |event, _, control_flow| {
-        if let Some(event) =
-            conrod_rendy::winit_convert::convert_event(event.clone(), &window)
-        {
+        if let Some(event) = conrod_rendy::winit_convert::convert_event(event.clone(), &window) {
             aux.ui.handle_event(event);
         }
 
