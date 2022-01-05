63548e72bc158cddabace230192fce60d2290939
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-01-06 00:10:41 +0900

    Make code compile

--- backends/conrod_sdl2/src/lib.rs
+++ backends/conrod_sdl2/src/lib.rs
@@ -1,3 +1,6 @@
+use conrod_core::{event::Input, input, Scalar};
+use sdl2::event::Event;
+
 /// A function for converting a `sdl2::event::Event` to a `conrod::event::Input`.
 ///
 /// This can be useful for single-window applications. Note that win_w and win_h should be the dpi
@@ -5,9 +8,8 @@
 ///
 /// NOTE: The sdl2 MouseMotion event is a combination of a MouseCursor and MouseRelative conrod
 /// events. Thus we may sometimes return two events in place of one, hence the tuple return type
-pub fn convert_event(e: event::Event, win_w: Scalar, win_h: Scalar) -> (Option<Input>, Option<Input>)
-{
-    use sdl2::event::{Event, WindowEvent};
+pub fn convert_event(e: Event, win_w: Scalar, win_h: Scalar) -> (Option<Input>, Option<Input>) {
+    use sdl2::event::WindowEvent;
 
     // Translate the coordinates from top-left-origin-with-y-down to centre-origin-with-y-up.
     //
@@ -17,47 +19,42 @@ pub fn convert_event(e: event::Event, win_w: Scalar, win_h: Scalar) -> (Option<I
     let ty = |y: f32| -((y as Scalar) - win_h / 2.0) as Scalar;
 
     match e {
-        Event::Window { win_event, .. } =>
-            (match win_event {
-                WindowEvent::Resized(w, h) => {
-                    Some(Input::Resize(w as u32, h as u32))
-                },
-
-                WindowEvent::FocusGained =>
-                    Some(Input::Focus(true)),
-
-                WindowEvent::FocusLost =>
-                    Some(Input::Focus(false)),
-
+        Event::Window { win_event, .. } => (
+            match win_event {
+                WindowEvent::Resized(w, h) => Some(Input::Resize(w as Scalar, h as Scalar)),
+                WindowEvent::FocusGained => Some(Input::Focus(true)),
+                WindowEvent::FocusLost => Some(Input::Focus(false)),
                 _ => None,
-            }, None),
-        Event::TextInput { text, .. } => {
-            (Some(Input::Text(text)), None)
-        },
-
-        Event::KeyDown { keycode: Some(key), .. } => {
-            (Some(Input::Press(input::Button::Keyboard(map_key(key)))), None)
-        },
-
-        Event::KeyUp { keycode: Some(key), .. } => {
-            (Some(Input::Release(input::Button::Keyboard(map_key(key)))), None)
-        },
-
+            },
+            None,
+        ),
+        Event::TextInput { text, .. } => (Some(Input::Text(text)), None),
+        Event::KeyDown {
+            keycode: Some(key), ..
+        } => (
+            Some(Input::Press(input::Button::Keyboard(map_key(key)))),
+            None,
+        ),
+        Event::KeyUp {
+            keycode: Some(key), ..
+        } => (
+            Some(Input::Release(input::Button::Keyboard(map_key(key)))),
+            None,
+        ),
         Event::FingerDown { touch_id, x, y, .. } => {
             let xy = [x as f64, y as f64];
             let id = input::touch::Id::new(touch_id as u64);
             let phase = input::touch::Phase::Start;
             let touch = input::Touch { phase, id, xy };
             (Some(Input::Touch(touch)), None)
-        },
-
+        }
         Event::FingerMotion { touch_id, x, y, .. } => {
             let xy = [x as f64, y as f64];
             let id = input::touch::Id::new(touch_id as u64);
             let phase = input::touch::Phase::Move;
             let touch = input::Touch { phase, id, xy };
             (Some(Input::Touch(touch)), None)
-        },
+        }
 
         Event::FingerUp { touch_id, x, y, .. } => {
             let xy = [x as f64, y as f64];
@@ -65,14 +62,20 @@ pub fn convert_event(e: event::Event, win_w: Scalar, win_h: Scalar) -> (Option<I
             let phase = input::touch::Phase::End;
             let touch = input::Touch { phase, id, xy };
             (Some(Input::Touch(touch)), None)
-        },
-
-        Event::MouseMotion { x, y, xrel, yrel, .. } => {
-            let cursor = input::Motion::MouseCursor { x: tx(x as f32), y: ty(y as f32) };
-            let relative = input::Motion::MouseRelative { x: tx(xrel as f32), y: ty(yrel as f32) };
+        }
+        Event::MouseMotion {
+            x, y, xrel, yrel, ..
+        } => {
+            let cursor = input::Motion::MouseCursor {
+                x: tx(x as f32),
+                y: ty(y as f32),
+            };
+            let relative = input::Motion::MouseRelative {
+                x: tx(xrel as f32),
+                y: ty(yrel as f32),
+            };
             (Some(Input::Motion(cursor)), Some(Input::Motion(relative)))
-        },
-
+        }
         Event::MouseWheel { x, y, .. } => {
             // Invert the scrolling of the *y* axis as *y* is up in conrod.
             const ARBITRARY_POINTS_PER_LINE_FACTOR: Scalar = 10.0;
@@ -80,23 +83,32 @@ pub fn convert_event(e: event::Event, win_w: Scalar, win_h: Scalar) -> (Option<I
             let y = ARBITRARY_POINTS_PER_LINE_FACTOR * (-y) as Scalar;
             let motion = input::Motion::Scroll { x, y };
             (Some(Input::Motion(motion)), None)
-        },
-
-        Event::MouseButtonDown { mouse_btn, .. } =>
-            (Some(Input::Press(input::Button::Mouse(map_mouse(mouse_btn)))), None),
-
-        Event::MouseButtonUp { mouse_btn, .. } =>
-            (Some(Input::Release(input::Button::Mouse(map_mouse(mouse_btn)))), None),
-
-        Event::JoyAxisMotion{ which, axis_idx, value, .. } => {
+        }
+        Event::MouseButtonDown { mouse_btn, .. } => (
+            Some(Input::Press(input::Button::Mouse(map_mouse(mouse_btn)))),
+            None,
+        ),
+        Event::MouseButtonUp { mouse_btn, .. } => (
+            Some(Input::Release(input::Button::Mouse(map_mouse(mouse_btn)))),
+            None,
+        ),
+        Event::JoyAxisMotion {
+            which,
+            axis_idx,
+            value,
+            ..
+        } => {
             // Axis motion is an absolute value in the range
             // [-32768, 32767]. Normalize it down to a float.
             use std::i16::MAX;
             let normalized_value = value as f64 / MAX as f64;
-            (Some(Input::Motion(input::Motion::ControllerAxis(ControllerAxisArgs::new(
-                            which, axis_idx, normalized_value)))), None)
+            (
+                Some(Input::Motion(input::Motion::ControllerAxis(
+                    input::ControllerAxisArgs::new(which, axis_idx, normalized_value),
+                ))),
+                None,
+            )
         }
-
         _ => (None, None),
     }
 }
@@ -108,22 +120,13 @@ pub fn map_key(keycode: sdl2::keyboard::Keycode) -> input::keyboard::Key {
 
 /// Maps sdl2's mouse button to conrod's mouse button.
 pub fn map_mouse(mouse_button: sdl2::mouse::MouseButton) -> input::MouseButton {
-    use input::MouseButton;
+    use conrod_core::input::MouseButton;
     match mouse_button {
         sdl2::mouse::MouseButton::Left => MouseButton::Left,
         sdl2::mouse::MouseButton::Right => MouseButton::Right,
         sdl2::mouse::MouseButton::Middle => MouseButton::Middle,
         sdl2::mouse::MouseButton::X1 => MouseButton::X1,
         sdl2::mouse::MouseButton::X2 => MouseButton::X2,
-        _ => MouseButton::Unknown
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        _ => MouseButton::Unknown,
     }
 }
